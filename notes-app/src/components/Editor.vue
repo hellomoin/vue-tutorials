cfe0706af39194dd1f3d42aa6be5164a1a171b90
msd <moin.salahuddin@gmail.com>
2017-03-09 15:00:17 +0100

    created vue templates for Editor, NotesList and Toolbar

---
+++ notes-app/src/components/Editor.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="note-editor">
+    <textarea :value="activeNoteText" @input="editNote" class="form-control">
+    </textarea>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ notes-app/src/components/Toolbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="toolbar">
+    <i @click="addNote" class="glyphicon glyphicon-plus"></i>
+    <i @click="toggleFavorite" class="glyphicon glyphicon-star" :class="{}"></i>
+    <i @click="deleteNote" class="glyphicon glyphicon-remove"></i>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
