2abe76630ef4cde7493bdcb17b9ded69391d0b19
msd <moin.salahuddin@gmail.com>
2017-03-10 16:12:34 +0100

    implemented vuex

---
+++ notes-app - Copy/src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <toolbar></toolbar>
+    <notes-list></notes-list>
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+import Toolbar from './Toolbar'
+import NotesList from './NotesList'
+import Editor from './Editor'
+
+export default {
+  name: 'app',
+  components: {
+    Toolbar, NotesList, Editor
+  }
+}
+</script>
+
+<style>
+
+</style>
