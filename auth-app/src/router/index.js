b200f391e26e149087ce5113070ac04c42ac09af
msd <moin.salahuddin@gmail.com>
2017-03-13 12:45:01 +0100

    define basic routes and created vue components

--- auth-app/src/router/index.js
+++ auth-app/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Login from '@/components/Login'
+import Signup from '@/components/Signup'
+import Dashboard from '@/components/Dashboard'
 
 Vue.use(Router)
 
@@ -10,6 +13,29 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/logout',
+      redirect: '/login',
+    },
+    {
+      path: '*',
+      redirect: '/home',
     }
   ]
 })
